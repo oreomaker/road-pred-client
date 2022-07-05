287d62a2fef5b5b2a95dd971b1dad5711897d755
oreomaker <zh002919@outlook.com>
2022-07-05 20:42:03 +0800

    style:统一缩进4space

--- src/components/layouts/BaseHeader.vue
+++ src/components/layouts/BaseHeader.vue
@@ -1,137 +1,136 @@
 <template>
-  <!-- 页面头部部分 -->
-  <div class="header">
-    <div class="logo">&nbsp;Logistic Management</div>
-    <!-- 水平一级菜单 -->
-    <div style="float: left">
-      <el-menu
-        class="tab-menu"
-        mode="horizontal"
-        text-color="black"
-        active-text-color="#3989fa"
-        :default-active="toIndex"
-        @select="handleSelect"
-      >
-        <el-menu-item
-          v-for="(item, index) in itemList"
-          :index="item.path"
-          :key="index"
-        >
-          {{ item.title }}
-        </el-menu-item>
-      </el-menu>
-    </div>
-    <!-- 用户图标部分 -->
-    <div class="header-right">
-      <div class="header-user-con">
-        <!-- 用户头像 -->
-        <el-icon><Avatar /></el-icon>
-        <!-- 用户名下拉菜单 -->
-        <el-dropdown class="user-name" trigger="hover" @command="handleCommand">
-          <span class="el-dropdown-link">
-            {{ username }}
-            <el-icon><ArrowDown /></el-icon>
-          </span>
-          <template #dropdown>
-            <el-dropdown-menu>
-              <el-dropdown-item command="change-pw">
-                <el-icon><Lock /></el-icon>
-                &nbsp;修改密码
-              </el-dropdown-item>
-              <el-dropdown-item command="log-out">
-                <el-icon><Switch /></el-icon>
-                &nbsp;注销
-              </el-dropdown-item>
-            </el-dropdown-menu>
-          </template>
-        </el-dropdown>
-      </div>
-    </div>
-  </div>
+	<!-- 页面头部部分 -->
+	<div class="header">
+		<div class="logo">&nbsp;Logistic Management</div>
+		<!-- 水平一级菜单 -->
+		<div style="float: left">
+			<el-menu class="tab-menu" mode="horizontal" text-color="black" active-text-color="#3989fa"
+				:default-active="toIndex" @select="handleSelect">
+				<el-menu-item v-for="(item, index) in itemList" :index="item.path" :key="index">
+					{{ item.title }}
+				</el-menu-item>
+			</el-menu>
+		</div>
+		<!-- 用户图标部分 -->
+		<div class="header-right">
+			<div class="header-user-con">
+				<!-- 用户头像 -->
+				<el-icon>
+					<Avatar />
+				</el-icon>
+				<!-- 用户名下拉菜单 -->
+				<el-dropdown class="user-name" trigger="hover" @command="handleCommand">
+					<span class="el-dropdown-link">
+						{{ username }}
+						<el-icon>
+							<ArrowDown />
+						</el-icon>
+					</span>
+					<template #dropdown>
+						<el-dropdown-menu>
+							<el-dropdown-item command="change-pw">
+								<el-icon>
+									<Lock />
+								</el-icon>
+								&nbsp;修改密码
+							</el-dropdown-item>
+							<el-dropdown-item command="log-out">
+								<el-icon>
+									<Switch />
+								</el-icon>
+								&nbsp;注销
+							</el-dropdown-item>
+						</el-dropdown-menu>
+					</template>
+				</el-dropdown>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 import { useAuthStore } from "~/stores/store.ts";
 
 export default {
-  data() {
-    return {
-      username: "",
-    };
-  },
-  computed: {
-    toIndex() {
-      // 根据路径绑定到对应的一级菜单，防止页面刷新重新跳回第一个
-      return "/" + this.$route.path.split("/")[1];
-    },
-    itemList() {
-      var role = useAuthStore().role;
-      console.log(role + " store");
-      return [
-        { path: "/" + role, title: "首页" },
-        { path: "/userinfo", title: "个人信息" },
-        { path: "/about", title: "关于" },
-      ];
-    },
-  },
-  methods: {
-    handleSelect(path) {
-      this.$router.push({
-        path: path,
-      });
-    },
-    handleCommand(command) {
-      if (command == "log-out") {
-        // clear the auth store
-        useAuthStore().token = "";
-        useAuthStore().role = "";
-        this.$router.push({
-          path: "/login",
-        });
-      } else if (command == "change-pw") {
-        // clear the auth store
-        this.$router.push({
-          path: "/change-pw",
-        });
-      }
-    },
-  },
-  mounted() {
-    this.username = useAuthStore().username;
-  },
+	data() {
+		return {
+			username: "",
+		};
+	},
+	computed: {
+		toIndex() {
+			// 根据路径绑定到对应的一级菜单，防止页面刷新重新跳回第一个
+			return "/" + this.$route.path.split("/")[1];
+		},
+		itemList() {
+			var role = useAuthStore().role;
+			console.log(role + " store");
+			return [
+				{ path: "/" + role, title: "首页" },
+				{ path: "/userinfo", title: "个人信息" },
+				{ path: "/about", title: "关于" },
+			];
+		},
+	},
+	methods: {
+		handleSelect(path) {
+			this.$router.push({
+				path: path,
+			});
+		},
+		handleCommand(command) {
+			if (command == "log-out") {
+				// clear the auth store
+				useAuthStore().token = "";
+				useAuthStore().role = "";
+				this.$router.push({
+					path: "/login",
+				});
+			} else if (command == "change-pw") {
+				// clear the auth store
+				this.$router.push({
+					path: "/change-pw",
+				});
+			}
+		},
+	},
+	mounted() {
+		this.username = useAuthStore().username;
+	},
 };
 </script>
 
 <style>
 .header {
-  height: 60px;
-  width: 100%;
-  background-color: white;
-  padding-left: 5px;
-  padding-right: 5px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+	height: 60px;
+	width: 100%;
+	background-color: white;
+	padding-left: 5px;
+	padding-right: 5px;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
 }
 
 .tab-menu {
-  width: 500px;
+	width: 500px;
 }
 
 .header-right {
-  float: right;
-  padding-right: 50px;
+	float: right;
+	padding-right: 50px;
 }
+
 .header-user-con {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 70px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 70px;
 }
 
 .el-dropdown-link {
-  cursor: pointer;
-  display: flex;
-  align-items: center;
+	cursor: pointer;
+	display: flex;
+	align-items: center;
 }
 </style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,32 +1,32 @@
 <template>
-  <div class="login-wrapper">
-    <LoginForm class="form" />
-    <ImageSwiper class="swiper" />
-  </div>
+    <div class="login-wrapper">
+        <LoginForm class="form" />
+        <ImageSwiper class="swiper" />
+    </div>
 </template>
 <script>
 export default {};
 </script>
 <style>
 body {
-  background-color: aliceblue;
+    background-color: aliceblue;
 }
 
 .login-wrapper {
-  height: 300px;
-  width: 700px;
-  display: flex;
-  justify-content: flex-start;
-  position: absolute;
-  top: 200px;
+    height: 300px;
+    width: 700px;
+    display: flex;
+    justify-content: flex-start;
+    position: absolute;
+    top: 200px;
 }
 
 .form {
-  width: 400px;
+    width: 400px;
 }
 
 .swiper {
-  width: 300px;
-  height: 100%;
+    width: 300px;
+    height: 100%;
 }
 </style>
