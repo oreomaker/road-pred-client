f57d38919f230427d8e5631caeff1df4ebee8569
oreomaker <zh002919@outlook.com>
2022-07-06 14:56:26 +0800

    feat:地图组件坐标获取和右键标识

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -20,9 +20,10 @@ export default {
         return {
             lngNum: null, // 经度
             latNum: null, // 纬度
+            map: null,
         }
     },
-    created: function () {
+    created() {
         let _this = this;
         initBingMap.init()
             .then((Microsoft) => {
@@ -34,13 +35,16 @@ export default {
     methods: {
         initMap() {
             let _this = this;
-            let map = new Microsoft.Maps.Map('#localMap', {
-                credentials: 'Aoqy2mXrSBghBTiA1u_uB6zmWqcHfdVJn4Jq7M7l87XgYlA7I0ZLSH5AlR19LzdY'
+            _this.map = new Microsoft.Maps.Map('#localMap', {
+                credentials: 'Aoqy2mXrSBghBTiA1u_uB6zmWqcHfdVJn4Jq7M7l87XgYlA7I0ZLSH5AlR19LzdY',
+                center: new Microsoft.Maps.Location(40.76737744930541, -73.98389818892902),
             });
-            Microsoft.Maps.Events.addHandler(map, 'click', _this.getClickLocation);
+            Microsoft.Maps.Events.addHandler(_this.map, 'click', _this.getClickLocation);
+            Microsoft.Maps.Events.addHandler(_this.map, 'rightclick', _this.rightClickMark);
         },
         getClickLocation(e) {
             //若点击到地图的标记上，而非地图上
+            console.log(e);
             let [_this, loc] = [this, null];
             if (e.targetType == 'pushpin') {
                 loc = e.target.getLocation();
@@ -50,7 +54,6 @@ export default {
                 var point = new Microsoft.Maps.Point(e.pageX, e.pageY);
                 loc = e.target.tryPixelToLocation(point, Microsoft.Maps.PixelReference.page);
             }
-            console.log(loc.latitude + ", " + loc.longitude);
             console.log(loc);
             _this.lngNum = loc.longitude;
             _this.latNum = loc.latitude;
@@ -60,6 +63,27 @@ export default {
             }
             this.$emit('getLocationNums', data);
         },
+        rightClickMark(e) {
+            console.log(e);
+            let [_this, loc] = [this, null];
+            if (e.targetType == 'pushpin') {
+                loc = e.target.getLocation();
+            }
+            //若点击到地图上
+            else {
+                var point = new Microsoft.Maps.Point(e.pageX, e.pageY);
+                loc = e.target.tryPixelToLocation(point, Microsoft.Maps.PixelReference.page);
+            }
+            //Add the pushpin to the map
+            var pin = new Microsoft.Maps.Pushpin(loc, {
+                // demo_1
+                title: 'Microsoft', // 图钉的标题
+                subTitle: 'City Center', // 图钉主体文字
+                text: '1', // 图钉内的文字
+                color: 'red', // 纯色图钉
+            });
+            _this.map.entities.push(pin);
+        }
     }
 }
 </script>
@@ -67,6 +91,7 @@ export default {
 .ep-header {
     padding: 0;
 }
+
 .map-container {
     width: 100%;
     height: 400px;
