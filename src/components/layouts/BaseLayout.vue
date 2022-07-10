32a84498c49a780dcf3c4c23f291060c7cffa0b6
joM4ker <joM4ker@aliyun.com>
2022-07-10 21:36:44 +0800

    补充上一次提交

---
+++ src/components/layouts/BaseLayout.vue
@@ -0,0 +1,32 @@
+<template>
+    <el-container>
+        <el-header>
+            <base-header />
+        </el-header>
+        <el-container class="body-container">
+            <el-aside width="200px" v-if="isLogin">
+                <BaseSide></BaseSide>
+            </el-aside>
+            <el-main>
+                <slot></slot>
+            </el-main>
+        </el-container>
+    </el-container>
+</template>
+
+<script setup lang="ts">
+import { ref,computed } from 'vue';
+import { useAuthStore } from '~/store';
+
+const store = useAuthStore();
+
+const isLogin = computed(() => {
+    return store.isLogin;
+})
+</script>
+
+<style>
+.ep-header {
+    padding: 0;
+}
+</style>
